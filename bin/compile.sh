d3dc929fd768dc29a7056c781ad45c315bf78250
Michael Snowden <msnowden@berkeley.edu>
2017-04-07 23:26:55 -0700

    forgot to add filename to end of path

--- bin/compile.sh
+++ bin/compile.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p "$BUILD_DIR/data"
-curl https://storage.googleapis.com/redditlsa/vectorized_data/index_to_sub.pkl > "$BUILD_DIR/data"
-curl https://storage.googleapis.com/redditlsa/vectorized_data/sub_to_index.pkl > "$BUILD_DIR/data"
-curl https://storage.googleapis.com/redditlsa/vectorized_data/subs_by_popularity.pkl > "$BUILD_DIR/data"
-curl https://storage.googleapis.com/redditlsa/vectorized_data/X.npz > "$BUILD_DIR/data"
+curl https://storage.googleapis.com/redditlsa/vectorized_data/index_to_sub.pkl > "$BUILD_DIR/data/index_to_sub.pkl"
+curl https://storage.googleapis.com/redditlsa/vectorized_data/sub_to_index.pkl > "$BUILD_DIR/data/sub_to_index.pkl"
+curl https://storage.googleapis.com/redditlsa/vectorized_data/subs_by_popularity.pkl > "$BUILD_DIR/data/subs_by_popularity.pkl"
+curl https://storage.googleapis.com/redditlsa/vectorized_data/X.npz > "$BUILD_DIR/data/X.npz"
